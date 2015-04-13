8ce74ccfc10e5bc2fa024436d604ef2ac679258d
Simon S. Soriano <simon0191@gmail.com>
2015-04-13 16:24:38 -0500

    Add Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :admins
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
