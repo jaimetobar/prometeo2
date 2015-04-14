4478b2bf0c8e87a0e784e7542bfad117e0cb9fb0
jasmo2 <jasmo2.com@gmail.com>
2015-04-14 16:56:28 -0500

    #1 CRUD for Courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,23 @@
+class CoursesController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
