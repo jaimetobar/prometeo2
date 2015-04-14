7b998bf86848476a82d3c8615797c083e3b8ef64
jasmo2 <jasmo2.com@gmail.com>
2015-04-14 11:48:55 -0500

    #1 add custom devise login

---
+++ lib/custom_failure/custom_failure.rb
@@ -0,0 +1,15 @@
+class CustomFailure < Devise::FailureApp
+  def redirect_url
+    new_admin_session_url(:subdomain => 'secure')
+  end
+
+  def respond
+    if http_auth?
+      http_auth
+    else
+      store_location!
+      flash[:alert] =  i18n_message(:invalid)
+      redirect_to :back 
+    end
+  end
+end
