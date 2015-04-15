fab07e5975330d081f2bdf5b46b001b71d322113
jasmo2 <jasmo2.com@gmail.com>
2015-04-15 12:31:05 -0500

    #1 Course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -18,9 +18,19 @@
 class Course < ActiveRecord::Base
   enum category: [:platform,:middleware,:cloud]
   enum session_type: [:always_available,:per_session]
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :category, presence: true
+  validates :session_type, presence: true
+ #acreditation is missing
+  validate :any_presence
 
   has_many :sessions, class_name: "CourseSession"
   has_many :subscriptions
   has_many :users, through: :subscriptions
 
+  def any_presence
+    if [:for_sales_engineer, :for_sales, :for_delivery].all?{|attr| self[attr].blank?}
+      errors.add(:any_presence,"You must fill in at least one field")
+    end
+  end
 end
