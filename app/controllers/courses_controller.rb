47d1db5e717bd88990f18f303be8f400add64699
jasmo2 <jasmo2.com@gmail.com>
2015-04-15 16:41:50 -0500

    #1 'edit' && 'update' methods for Courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :get_course, only: [:destroy, :update, :edit]
   def index
     @courses = Course.all
     # byebug
@@ -23,15 +24,28 @@ class CoursesController < ApplicationController
   def show
   end
 
-  def update
-  end
-
   def edit
   end
 
-  def destroy
+  def update
+    if @course.update(courses_params)
+      redirect_to courses_path
+    else
+      flash[:any_presence] = @course.errors[:any_presence][0]
+      render :edit
+    end
   end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_path
+  end
+
+
   private
+  def get_course
+    @course = Course.find(params[:id])
+  end
   def courses_params
     params.require(:course).permit(:name,:category,:description,
       :for_sales_engineer,:for_sales,:for_delivery,:session_type,:accreditation_id)    
