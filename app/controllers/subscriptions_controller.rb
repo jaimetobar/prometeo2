1252b2a0909704d9a6e64dfb0b66d688a92e4be0
Simon S. Soriano <simon0191@gmail.com>
2015-04-23 10:48:12 -0500

    #1 Fix user link

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,13 +1,18 @@
 class SubscriptionsController < ApplicationController
-  
+
   def index
-    @subscriptions = Subscription.where(user_id: params[:user_id]).order('id asc')
-    @user = Subscription.find(params[:user_id]))
+    @user = User.find(params[:user_id])
+    @subscriptions = Subscription.where(user: @user).order('id asc')
   end
 
   def multiple
     Subscription.update_multiple(params[:user_id],params[:notifications_on],params[:finished])
 
     redirect_to user_subscriptions_path
+  end
+
+  protected
+  def subscription_params
+    params.required(:subscription).permit(:id,:user_id)
   end
 end
