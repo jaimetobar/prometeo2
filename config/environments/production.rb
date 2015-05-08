947145dd3474d3bffcc0fad44773f3fbdcae678f
Simon S. Soriano <simon0191@gmail.com>
2015-05-08 04:12:01 -0500

    #25 setup provisional email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address              => ENV['PROMETEO_ADDRESS'],
-      :port                 => 587,
-      :domain               => ENV['PROMETEO_DOMAIN'],
-      :user_name            => ENV['PROMETEO_USER'],
-      :password             => ENV['PROMETEO_PASSWORD'],
-      :authentication       => 'plain',
+      :address              => ENV['SMTP_ADDRESS'],
+      :port                 => ENV['SMTP_PORT'],
+      :domain               => ENV['SMTP_DOMAIN'],
+      :user_name            => ENV['SMTP_USER'],
+      :password             => ENV['SMTP_PASSWORD'],
+      :authentication       => ENV['SMTP_AUTHENTICATION'],
       :enable_starttls_auto => true
   }
   config.action_mailer.perform_deliveries = true
