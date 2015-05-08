80b5a29561773ae0da0495429a613dfac0ca346d
Simon S. Soriano <simon0191@gmail.com>
2015-05-08 03:36:22 -0500

    #22 Add user pagination

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508075023) do
+ActiveRecord::Schema.define(version: 20150508081704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "accreditations", force: true do |t|
     t.string   "name"
