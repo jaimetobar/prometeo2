b8065a16019d373647c998c54dcb6625ec6c411e
Simon S. Soriano <simon0191@gmail.com>
2015-05-14 22:02:36 -0500

    Fix sidekiq configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
 if Rails.env == "production"
   Sidekiq.configure_server do |config|
     config.redis = {
-      url: "redis://#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV[OPENSHIFT_REDIS_PORT]}/12",
+      url: "redis://#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}/12",
       password: "#{ENV['REDIS_PASSWORD']}"
     }
   end
 
   Sidekiq.configure_client do |config|
     config.redis = {
-      url: "redis://#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV[OPENSHIFT_REDIS_PORT]}/12",
+      url: "redis://#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}/12",
       password: "#{ENV['REDIS_PASSWORD']}"
     }
   end
