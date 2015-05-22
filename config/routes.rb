ff3265cc6016ffdb366cdc61cb91f074d0e807c6
Simon S. Soriano <simon0191@gmail.com>
2015-05-22 00:35:46 -0500

    Allow admins to change password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root "plan#index"
 
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    patch 'admins/:id' => 'devise/registrations#update', :as => 'admin_registration'
+  end
 
   resource :plan, only:[:index,:create], controller: :plan do
     collection do
