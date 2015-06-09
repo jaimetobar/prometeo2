679fc40101d1fdd58f8047deb602fb4bbeb558aa
jasmo2 <jasmo2.com@gmail.com>
2015-06-09 16:07:07 -0500

    #7 admin login spec

---
+++ spec/features/admin_spec.rb
@@ -0,0 +1,19 @@
+require'rails_helper'
+
+describe "sign into admin panel" do
+  context 'login credentials' do
+    let!(:admin) { create(:admin) }
+    before :each do    
+      visit new_admin_session_path
+    end
+    it 'check for valid credentials' do
+      within("#new_admin") do
+        fill_in 'admin_email', with: admin.email
+        fill_in 'admin_password', with: admin.password
+        click_on 'Ingresar'
+      end
+      visit admins_path
+      expect(page).to have_content("Administradores")
+    end
+  end
+end
