292d7a305608f189c437aad6d44943c3b75d2e10
Simon S. Soriano <simon0191@gmail.com>
2015-08-04 22:57:42 -0500

    Add next date to CSV subscriptions report

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def next_session(course)
+    return "Siempre disponible" if course.always_available?
+
+    if course.next_session.nil?
+      "No disponible"
+    else
+      course.next_session.start_date
+    end
+
+  end
 end
