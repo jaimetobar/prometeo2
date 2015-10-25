c89e18e5a9c72db7c7ffe4da1c5351e12f46eee8
Simon S. Soriano <simon0191@gmail.com>
2015-10-24 19:16:11 -0500

    Customize emails for MX and BR

--- app/mailers/plan_mailer.rb
+++ app/mailers/plan_mailer.rb
@@ -4,10 +4,19 @@ class PlanMailer < ApplicationMailer
   def plan_greatings_email(user)
     @user = user
     @sessions_per_course = Course.sessions_per_course(user.courses)
+    template_name = case user.country
+    when "BR"
+      "plan_greatings_email_br"
+    when "MX"
+      "plan_greatings_email_mx"
+    else
+      "plan_greatings_email"
+    end
     mail(
       from: sender_for(@user),
       to: @user.email,
-      subject: 'Plan de Acreditaciones de Red Hat')
+      subject: 'Plan de Acreditaciones de Red Hat',
+      template_name: template_name)
   end
 
   def upcoming_courses_email(user, subscriptions_ids ,start_date)
