ab46883187d9de4e5af1c69a22e0d0457bc81eba
Simon S. Soriano <simon0191@gmail.com>
2016-02-01 13:44:40 -0500

    Add translations for plan greatings email

--- app/mailers/plan_mailer.rb
+++ app/mailers/plan_mailer.rb
@@ -4,19 +4,17 @@ class PlanMailer < ApplicationMailer
   def plan_greatings_email(user)
     @user = user
     @sessions_per_course = Course.sessions_per_course(user.courses)
-    template_name = case user.country
-    when "BR"
-      "plan_greatings_email_br"
-    when "MX"
-      "plan_greatings_email_mx"
-    else
-      "plan_greatings_email"
-    end
+    template_name = template_for_country(@user.country)
+# TODO: Use locales for email text translation
+#    I18n.with_locale(@user.profile.locale) do
+#      mail :to=>@user.email
+#    end
     mail(
       from: sender_for(@user),
       to: @user.email,
       subject: 'Plan de Acreditaciones de Red Hat',
       template_name: template_name)
+
   end
 
   def upcoming_courses_email(user, subscriptions_ids ,start_date)
@@ -34,12 +32,35 @@ class PlanMailer < ApplicationMailer
   end
 
   private
-  def sender_for(user)
-    case user.country
-    when "MX"
-      "monicaos@redhat.com"
-    else
-      "cromero@redhat.com"
+    def sender_for(user)
+      case user.country
+      when "MX"
+        "monicaos@redhat.com"
+      else
+        "cromero@redhat.com"
+      end
     end
-  end
+
+    def template_for_country(country)
+      template_suffix = case country
+      when 'BR'
+        '_br'
+      when 'MX'
+        '_mx'
+      when 'CN'
+        '_zh'
+      when 'JP'
+        '_jp'
+      when 'KP','KR'
+        '_kr'
+      else
+        southamerican_countries = ISO3166::Country.find_all_by(:subregion,'South America')
+        if southamerican_countries.include? country
+          '_es'
+        else
+          ''
+        end
+      end
+      template_name = "plan_greatings_email#{template_suffix}"
+    end
 end
